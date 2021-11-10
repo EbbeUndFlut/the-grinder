b228b184f312c36c1f0e0c1fe3f37edced6f2b28
CryptoChris <c.peters@makermaker.de>
2021-11-10 22:12:05 +0100

    some

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ import KeyControls from "./controlls/KeyControls.js"
 import Container from "./container.js"
 import CanvasRenderer from "./renderer/CanvasRenderer.js"
 import Text from "./Text.js"
+export default { MouseControls, KeyControls, Container, CanvasRenderer, Text }
 
-export default {
-	MouseControls,
-	KeyControls,
-	Container,
-	CanvasRenderer,
-	Text,
-}
+// exports.MouseControls = MouseControls
+// exports.Container = Container
+// exports.CanvasRenderer = CanvasRenderer
+// exports.Text = Text
